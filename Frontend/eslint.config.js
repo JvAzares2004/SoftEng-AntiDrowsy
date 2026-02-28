91e9f6fd15b8f8484648ccdcc7a2f9a626a16a95
Matthew Cania <matthewgab24@gmail.com>
2026-02-28 23:32:41 +0800

    Fixed types/react

--- Frontend/eslint.config.js
+++ Frontend/eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Explicit any → error
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
   },
 ])
